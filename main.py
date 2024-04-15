c0616dcafb6673383b5cb2c079b4dfc2a7a0b4d8
Meruvu Likith <110189869+LikithMeruvu@users.noreply.github.com>
2024-04-15 17:22:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,33 @@ from src.News_Reporter import NewsAnalysisEngine
 from src.Blog_Generator import BlogPostGenerator
 from datetime import datetime
 
+def generate_news_and_blog(topic):
+    """
+    Generates news analysis and blog post for the given topic.
 
-engine = NewsAnalysisEngine()
-blog_post_generator = BlogPostGenerator()
-d = datetime.now().strftime("%Y-%m-%d")
-result = engine.analyze_news(f"Generative AI news and advancements today's Date is :-{d}")
-print(result)
+    Args:
+        topic (str): The topic for which the news analysis and blog post should be generated.
 
+    Returns:
+        str: The news analysis result.
+        str: The generated blog post.
+    """
+    engine = NewsAnalysisEngine()
+    blog_post_generator = BlogPostGenerator()
+
+    d = datetime.now().strftime("%Y-%m-%d")
+
+    result = engine.analyze_news(f"{topic} today's Date is :-{d}")
+
+    blog_post = blog_post_generator.generate_blog_post(result)
+
+    return result, blog_post
+
+
+news_analysis, blog_post = generate_news_and_blog("Generative AI news and advancements")
+print(news_analysis)
+print("\n\n\n\n\n")
 print("================================This is The Actual Blog post +++++++++++++++++++++++++")
-blog_post = blog_post_generator.generate_blog_post(result)
 print(blog_post)
-
 
 
